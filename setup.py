e43d0fbacbc1639f032e4cf4981c503641b958ee
David McClure <davidwilliammcclure@gmail.com>
2017-12-02 11:05:42 -0500

    Confirm KT sampling.

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sent-order',
+    version='0.1.0',
+    description='Sentence ordering.',
+    url='https://github.com/davidmcclure/sent-order',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@mit.edu',
+    packages=find_packages(),
+)
