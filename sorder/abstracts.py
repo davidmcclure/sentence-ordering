bd100eb0c0fef0c0b6c5fa4ab76a3b78f2677bc9
David McClure <davidwilliammcclure@gmail.com>
2017-12-04 13:54:39 -0500

    Break out abstracts module.

---
+++ sorder/abstracts.py
@@ -0,0 +1,94 @@
+
+
+import attr
+import os
+import ujson
+import random
+import torch
+
+import numpy as np
+
+from glob import glob
+from itertools import islice
+from tqdm import tqdm
+
+from sorder.vectors import LazyVectors
+
+
+vectors = LazyVectors.read()
+
+
+def read_abstracts(path, maxlen):
+    """Parse abstract JSON lines.
+    """
+    for path in glob(os.path.join(path, '*.json')):
+        with open(path) as fh:
+            for line in fh:
+                json = ujson.loads(line.strip())
+                if len(json['sentences']) < maxlen:
+                    yield Abstract.from_json(json)
+
+
+@attr.s
+class Sentence:
+
+    tokens = attr.ib()
+
+    def tensor(self, dim=300, pad=50):
+        """Stack word vectors, padding zeros on left.
+        """
+        x = [vectors[t] for t in self.tokens if t in vectors]
+        x += [np.zeros(dim)] * pad
+        x = x[:pad]
+        x = list(reversed(x))
+        x = np.array(x)
+        x = torch.from_numpy(x)
+        x = x.float()
+        return x
+
+
+@attr.s
+class Abstract:
+
+    sentences = attr.ib()
+
+    @classmethod
+    def from_json(cls, json):
+        """Pull out raw token series.
+        """
+        return cls([Sentence(s['token']) for s in json['sentences']])
+
+    def tensor(self):
+        """Stack sentence tensors.
+        """
+        tensors = [s.tensor() for s in self.sentences]
+        return torch.stack(tensors)
+
+
+@attr.s
+class AbstractBatch:
+
+    abstracts = attr.ib()
+
+    def tensor(self):
+        """Stack abstract tensors.
+        """
+        tensors = [a.tensor() for a in self.abstracts]
+        return torch.cat(tensors)
+
+
+class Corpus:
+
+    def __init__(self, path, skim=None, maxlen=10):
+        """Load abstracts into memory.
+        """
+        reader = read_abstracts(path, maxlen)
+
+        if skim:
+            reader = islice(reader, skim)
+
+        self.abstracts = list(tqdm(reader, total=skim))
+
+    def random_batches(self, n, size):
+        for _ in tqdm(range(n)):
+            yield random.sample(self.abstracts, size)
