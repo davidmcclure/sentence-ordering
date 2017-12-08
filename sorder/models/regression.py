a5db33e221e247055a182993b02c82561fab672f
David McClure <davidwilliammcclure@gmail.com>
2017-12-08 14:00:37 -0500

    Re-combine regression components.

--- sorder/models/regression.py
+++ sorder/models/regression.py
@@ -1,18 +1,107 @@
 
 
+import numpy as np
+
+import os
 import click
 import torch
-import os
+import attr
+import random
+import ujson
+
+from tqdm import tqdm
+from itertools import islice
+from glob import glob
+from scipy import stats
 
 from torch import nn
 from torch.autograd import Variable
 from torch.nn import functional as F
-from tqdm import tqdm
-from scipy import stats
 
 from sorder.cuda import CUDA, ftype, itype
-from sorder.abstracts import Corpus
 from sorder.utils import checkpoint
+from sorder.vectors import LazyVectors
+
+
+vectors = LazyVectors.read()
+
+
+def read_abstracts(path, maxlen):
+    """Parse abstract JSON lines.
+    """
+    for path in glob(os.path.join(path, '*.json')):
+        with open(path) as fh:
+            for line in fh:
+                json = ujson.loads(line.strip())
+                if len(json['sentences']) < maxlen:
+                    yield Abstract.from_json(json)
+
+
+@attr.s
+class Sentence:
+
+    tokens = attr.ib()
+
+    def tensor(self, dim=300, pad=50):
+        """Stack word vectors, padding zeros on left.
+        """
+        x = [vectors[t] for t in self.tokens if t in vectors]
+        x += [np.zeros(dim)] * pad
+        x = x[:pad]
+        x = list(reversed(x))
+        x = np.array(x)
+        x = torch.from_numpy(x)
+        x = x.float()
+        return x
+
+
+@attr.s
+class Abstract:
+
+    sentences = attr.ib()
+
+    @classmethod
+    def from_json(cls, json):
+        """Pull out raw token series.
+        """
+        return cls([Sentence(s['token']) for s in json['sentences']])
+
+    def tensor(self):
+        """Stack sentence tensors.
+        """
+        tensors = [s.tensor() for s in self.sentences]
+        return torch.stack(tensors)
+
+
+@attr.s
+class AbstractBatch:
+
+    abstracts = attr.ib()
+
+    def tensor(self):
+        """Stack abstract tensors.
+        """
+        tensors = [a.tensor() for a in self.abstracts]
+        return torch.cat(tensors)
+
+
+class Corpus:
+
+    def __init__(self, path, skim=None, maxlen=10):
+        """Load abstracts into memory.
+        """
+        reader = read_abstracts(path, maxlen)
+
+        if skim:
+            reader = islice(reader, skim)
+
+        self.abstracts = list(tqdm(reader, total=skim))
+
+    def random_batch(self, size):
+        return random.sample(self.abstracts, size)
+
+    def batches(self, size):
+        yield from chunked_iter(self.abstracts, size)
 
 
 class SentenceEncoder(nn.Module):
@@ -23,7 +112,7 @@ class SentenceEncoder(nn.Module):
 
     def forward(self, x):
         _, (hn, cn) = self.lstm(x)
-        return hn[-1].squeeze()
+        return hn.squeeze()
 
     def encode_batch(self, batch):
         """Encode sentences in an abstract batch.
