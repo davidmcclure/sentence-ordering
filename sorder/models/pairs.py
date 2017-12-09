d65e4613c2b9214bdd56c119681a5a3806f4672c
David McClure <davidwilliammcclure@gmail.com>
2017-12-09 12:22:45 -0500

    Pairs readers.

--- sorder/models/pairs.py
+++ sorder/models/pairs.py
@@ -1,3 +1,102 @@
 
 
-# TODO
+import numpy as np
+
+import os
+import click
+import torch
+import attr
+import random
+import ujson
+
+from tqdm import tqdm
+from itertools import islice
+from glob import glob
+from scipy import stats
+
+from torch import nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torch.autograd import Variable
+from torch.nn import functional as F
+
+from sorder.cuda import CUDA, ftype, itype
+from sorder.utils import checkpoint
+from sorder.vectors import LazyVectors
+
+
+vectors = LazyVectors.read()
+
+
+def read_abstracts(path, maxlen):
+    """Parse abstract JSON lines.
+    """
+    for path in glob(os.path.join(path, '*.json')):
+        with open(path) as fh:
+            for line in fh:
+
+                # Parse JSON.
+                abstract = Abstract.from_line(line)
+
+                # Filter by length.
+                if len(abstract.sentences) < maxlen:
+                    yield abstract
+
+
+@attr.s
+class Sentence:
+
+    tokens = attr.ib()
+
+    def tensor(self, dim=300, pad=50):
+        """Stack word vectors, padding zeros on left.
+        """
+        # Get word tensors and length.
+        x = [vectors[t] for t in self.tokens if t in vectors]
+        size = min(len(x), pad)
+
+        # Pad zeros.
+        x += [np.zeros(dim)] * pad
+        x = x[:pad]
+        x = np.array(x)
+        x = torch.from_numpy(x)
+        x = x.float()
+        return x, size
+
+
+@attr.s
+class Abstract:
+
+    sentences = attr.ib()
+
+    @classmethod
+    def from_line(cls, line):
+        """Parse JSON, take tokens.
+        """
+        json = ujson.loads(line.strip())
+
+        return cls([
+            Sentence(s['token'])
+            for s in json['sentences']
+        ])
+
+
+@attr.s
+class Batch:
+
+    abstracts = attr.ib()
+
+
+class Corpus:
+
+    def __init__(self, path, skim=None, maxlen=10):
+        """Load abstracts into memory.
+        """
+        reader = read_abstracts(path, maxlen)
+
+        if skim:
+            reader = islice(reader, skim)
+
+        self.abstracts = list(tqdm(reader, total=skim))
+
+    def random_batch(self, size):
+        return Batch(random.sample(self.abstracts, size))
--- sorder/utils.py
+++ sorder/utils.py
@@ -1,7 +1,11 @@
 
 
+import numpy as np
+
 import os
 import torch
+
+from torch.nn.utils.rnn import pack_padded_sequence
 
 
 def checkpoint(root, key, model, epoch):
@@ -10,3 +14,18 @@ def checkpoint(root, key, model, epoch):
     os.makedirs(root, exist_ok=True)
     path = os.path.join(root, f'{key}.{epoch}.pt')
     torch.save(model, path)
+
+
+def pack(tensor, sizes, batch_first=True):
+    """Pack padded tensors, provide reorder indexes.
+    """
+    # Get indexes for sorted sizes.
+    size_sort = np.argsort(sizes)[::-1].tolist()
+
+    # Sort the tensor / sizes.
+    tensor = tensor[torch.LongTensor(size_sort)].type(ftype)
+    sizes = np.array(sizes)[size_sort].tolist()
+
+    packed = pack_padded_sequence(Variable(tensor), sizes, batch_first)
+
+    return packed, size_sort
