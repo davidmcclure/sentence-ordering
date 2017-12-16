70ccb13ee44d4de4ffd20089f4233c9c9bd4991b
David McClure <davidwilliammcclure@gmail.com>
2017-12-16 14:43:34 -0500

    Metrics.

--- sorder/metrics.py
+++ sorder/metrics.py
@@ -1,9 +1,11 @@
 
 
+import numpy as np
+
 import attr
 
-from collections import defaultdict
 from cached_property import cached_property
+from collections import defaultdict, Counter
 
 from scipy import stats
 
@@ -14,8 +16,15 @@ class Metrics:
     gold_pred = attr.ib()
 
     @cached_property
-    def kts_by_sent_count(self):
-        """Kendall's tau by sentence length.
+    def len_counts(self):
+        """Sentence count -> count.
+        """
+        lengths = map(len, [g for g, _ in self.gold_pred])
+        return Counter(list(lengths))
+
+    @cached_property
+    def kts_by_len(self):
+        """Kendall's tau by sentence count.
         """
         kts = defaultdict(list)
 
@@ -26,24 +35,44 @@ class Metrics:
         return kts
 
     @cached_property
-    def avg_kt_by_sent_count(self):
+    def avg_kt_by_len(self):
         """Average KT for each sentence count.
         """
         return {
             count: sum(kts) / len(kts)
-            for count, kts in self.kts_by_sent_count.items()
+            for count, kts in self.kts_by_len.items()
         }
 
     @cached_property
     def all_kts(self):
         """Set of all KT scores.
         """
-        by_count = self.kts_by_sent_count.values()
-
-        return [kt for kts in by_count for kt in kts]
+        return [kt for kts in self.kts_by_len.values() for kt in kts]
 
     @cached_property
-    def avg_kt(self):
+    def overall_kt(self):
         """Overall average KT.
         """
         return sum(self.all_kts) / len(self.all_kts)
+
+    @cached_property
+    def perfect_order_pct_by_len(self):
+        """Percent perfect order by sentence count.
+        """
+        perfect = Counter()
+
+        for gold, pred in self.gold_pred:
+            if gold == pred:
+                perfect[len(gold)] += 1
+
+        return {
+            slen: perfect.get(slen, 0) / self.len_counts[slen]
+            for slen in self.len_counts
+        }
+
+    @cached_property
+    def overall_perfect_order_pct(self):
+        """Percent perfect order overall.
+        """
+        perfect = sum([1 for g, p in self.gold_pred if g==p])
+        return perfect / len(self.gold_pred)
