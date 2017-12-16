da022e54717db1f30a4196935f611127f237bbb2
David McClure <davidwilliammcclure@gmail.com>
2017-12-16 16:40:53 -0500

    LR figures.

--- sorder/metrics.py
+++ sorder/metrics.py
@@ -3,17 +3,22 @@
 import numpy as np
 
 import attr
+import warnings
 
 from cached_property import cached_property
-from collections import defaultdict, Counter
-
+from collections import defaultdict, Counter, OrderedDict
 from scipy import stats
 
+from .utils import sort_by_key
 
-@attr.s
+
+warnings.simplefilter("ignore")
+
+
 class Metrics:
 
-    gold_pred = attr.ib()
+    def __init__(self, gold_pred):
+        self.gold_pred = gold_pred
 
     @cached_property
     def len_counts(self):
@@ -21,6 +26,27 @@ class Metrics:
         """
         lengths = map(len, [g for g, _ in self.gold_pred])
         return Counter(list(lengths))
+
+    def perfect_order_pct_by_len(self, max_len=10):
+        """Percent perfect order by sentence count.
+        """
+        perfect = Counter()
+
+        for gold, pred in self.gold_pred:
+            if gold == pred:
+                perfect[len(gold)] += 1
+
+        return sort_by_key({
+            slen: perfect.get(slen, 0) / self.len_counts[slen]
+            for slen in self.len_counts
+            if slen <= max_len
+        })
+
+    def overall_perfect_order_pct(self):
+        """Percent perfect order overall.
+        """
+        perfect = sum([1 for g, p in self.gold_pred if g==p])
+        return perfect / len(self.gold_pred)
 
     @cached_property
     def kts_by_len(self):
@@ -34,14 +60,14 @@ class Metrics:
 
         return kts
 
-    @cached_property
-    def avg_kt_by_len(self):
+    def avg_kt_by_len(self, max_len=10):
         """Average KT for each sentence count.
         """
-        return {
-            count: sum(kts) / len(kts)
-            for count, kts in self.kts_by_len.items()
-        }
+        return sort_by_key({
+            slen: sum(kts) / len(kts)
+            for slen, kts in self.kts_by_len.items()
+            if slen <= max_len
+        })
 
     @cached_property
     def all_kts(self):
@@ -49,30 +75,32 @@ class Metrics:
         """
         return [kt for kts in self.kts_by_len.values() for kt in kts]
 
-    @cached_property
     def overall_kt(self):
         """Overall average KT.
         """
         return sum(self.all_kts) / len(self.all_kts)
 
-    @cached_property
-    def perfect_order_pct_by_len(self):
-        """Percent perfect order by sentence count.
+    def positional_accuracy_pct_by_len(self, max_len=10):
+        """Percentage of sentences in the right slot.
         """
-        perfect = Counter()
+        correct = Counter()
 
         for gold, pred in self.gold_pred:
-            if gold == pred:
-                perfect[len(gold)] += 1
+            correct[len(gold)] += np.equal(gold, pred).sum()
 
         return sort_by_key({
-            slen: perfect.get(slen, 0) / self.len_counts[slen]
+            slen: correct.get(slen, 0) / (self.len_counts[slen] * slen)
             for slen in self.len_counts
+            if slen <= max_len
         })
 
-    @cached_property
-    def overall_perfect_order_pct(self):
-        """Percent perfect order overall.
+    def overall_positional_accuracy_pct(self):
+        """Positional accuracy for all sentences.
         """
-        perfect = sum([1 for g, p in self.gold_pred if g==p])
-        return perfect / len(self.gold_pred)
+        c, t = 0, 0
+
+        for gold, pred in self.gold_pred:
+            c += np.equal(gold, pred).sum()
+            t += len(gold)
+
+        return c / t
