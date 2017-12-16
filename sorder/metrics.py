ea52617344394863efc3f5c55193ccb5ef96280c
David McClure <davidwilliammcclure@gmail.com>
2017-12-16 11:39:28 -0500

    LR fit.

--- sorder/metrics.py
+++ sorder/metrics.py
@@ -3,6 +3,7 @@
 import attr
 
 from collections import defaultdict
+from cached_property import cached_property
 
 
 @attr.s
@@ -10,6 +11,7 @@ class Metrics:
 
     gold_pred = attr.ib()
 
+    @cached_property
     def kts_by_sent_count(self):
         """Kendall's tau by sentence length.
         """
@@ -20,3 +22,26 @@ class Metrics:
             kts[len(gold)].append(kt)
 
         return kts
+
+    @cached_property
+    def avg_kt_by_sent_count(self):
+        """Average KT for each sentence count.
+        """
+        return {
+            count: sum(kts) / len(kts)
+            for count, kts in self.kts_by_sent_count
+        }
+
+    @cached_property
+    def all_kts(self):
+        """Set of all KT scores.
+        """
+        by_count = self.kts_by_sent_count.values()
+
+        return [kt for kts in by_count for kt in kts]
+
+    @cached_property
+    def avg_kt(self):
+        """Overall average KT.
+        """
+        return sum(self.all_kts) / len(self.all_kts)
