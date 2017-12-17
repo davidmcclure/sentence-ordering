ee0a6a8e6fffee42a49bdf6c1be9aca22076e164
David McClure <davidwilliammcclure@gmail.com>
2017-12-16 21:48:13 -0500

    Ready for runs.

--- bin/pick_next.py
+++ bin/pick_next.py
@@ -14,15 +14,16 @@ def cli():
 @cli.command()
 @click.argument('train_path', type=click.Path())
 @click.argument('model_path', type=click.Path())
-@click.option('--train_skim', type=int, default=10000)
+@click.option('--train_skim', type=int, default=1000000)
 @click.option('--lr', type=float, default=1e-3)
 @click.option('--epochs', type=int, default=1000)
-@click.option('--epoch_size', type=int, default=100)
+@click.option('--epoch_size', type=int, default=1000)
 @click.option('--batch_size', type=int, default=20)
 @click.option('--lstm_dim', type=int, default=500)
 @click.option('--lin_dim', type=int, default=500)
+@click.option('--gpu', type=int, default=2)
 def train(*args, **kwargs):
-    with cuda.gpu(2):
+    with cuda.gpu(kwargs.pop('gpu')):
         model.train(*args, **kwargs)
 
 
