cd667440627ab7cf58e1a2c0ba88e9ebb96ce14a
David McClure <davidwilliammcclure@gmail.com>
2017-12-16 21:08:43 -0500

    Pairs.

---
+++ bin/pairs.py
@@ -0,0 +1,28 @@
+
+
+import click
+
+from sorder.models import pairs as model
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('train_path', type=click.Path())
+@click.argument('model_path', type=click.Path())
+@click.option('--train_skim', type=int, default=10000)
+@click.option('--lr', type=float, default=1e-3)
+@click.option('--epochs', type=int, default=1000)
+@click.option('--epoch_size', type=int, default=100)
+@click.option('--batch_size', type=int, default=20)
+@click.option('--lstm_dim', type=int, default=500)
+@click.option('--lin_dim', type=int, default=500)
+def train(*args, **kwargs):
+    model.train(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    cli()
---
+++ sorder/models/pairs.py
@@ -0,0 +1,236 @@
+
+
+import numpy as np
+
+import os
+import click
+import torch
+import attr
+import random
+import ujson
+import math
+
+from tqdm import tqdm
+from itertools import islice
+from glob import glob
+from boltons.iterutils import pairwise, chunked_iter
+from scipy import stats
+
+from torch import nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.autograd import Variable
+from torch.nn import functional as F
+
+from sorder.utils import checkpoint, pad_and_pack
+from sorder.vectors import LazyVectors
+from sorder.cuda import CUDA, ftype, itype
+
+
+vectors = LazyVectors.read()
+
+
+def read_abstracts(path):
+    """Parse abstract JSON lines.
+    """
+    for path in glob(os.path.join(path, '*.json')):
+        with open(path) as fh:
+            for line in fh:
+                yield Abstract.from_line(line)
+
+
+@attr.s
+class Sentence:
+
+    tokens = attr.ib()
+
+    def tensor(self, dim=300):
+        """Stack word vectors.
+        """
+        x = [
+            vectors[t] if t in vectors else np.zeros(dim)
+            for t in self.tokens
+        ]
+
+        x = np.array(x)
+        x = torch.from_numpy(x)
+        x = x.float()
+
+        return x
+
+
+@attr.s
+class Abstract:
+
+    sentences = attr.ib()
+
+    @classmethod
+    def from_line(cls, line):
+        """Parse JSON, take tokens.
+        """
+        json = ujson.loads(line.strip())
+
+        return cls([
+            Sentence(s['token'])
+            for s in json['sentences']
+        ])
+
+
+@attr.s
+class Batch:
+
+    abstracts = attr.ib()
+
+    def packed_sentence_tensor(self, size=50):
+        """Pack sentence tensors.
+        """
+        sents = [
+            Variable(s.tensor()).type(ftype)
+            for a in self.abstracts
+            for s in a.sentences
+        ]
+
+        return pad_and_pack(sents, size)
+
+    def unpack_sentences(self, encoded):
+        """Unpack encoded sentences.
+        """
+        start = 0
+        for ab in self.abstracts:
+            end = start + len(ab.sentences)
+            yield encoded[start:end]
+            start = end
+
+
+class Corpus:
+
+    def __init__(self, path, skim=None):
+        """Load abstracts into memory.
+        """
+        reader = read_abstracts(path)
+
+        if skim:
+            reader = islice(reader, skim)
+
+        self.abstracts = list(tqdm(reader, total=skim))
+
+    def random_batch(self, size):
+        """Query random batch.
+        """
+        return Batch(random.sample(self.abstracts, size))
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, input_dim, lstm_dim):
+        super().__init__()
+        self.lstm = nn.LSTM(input_dim, lstm_dim, batch_first=True,
+            bidirectional=True)
+
+    def forward(self, x, reorder):
+        _, (hn, _) = self.lstm(x)
+        # Cat forward + backward hidden layers.
+        out = hn.transpose(0, 1).contiguous().view(hn.data.shape[1], -1)
+        return out[reorder]
+
+
+class Classifier(nn.Module):
+
+    def __init__(self, input_dim, lin_dim):
+        super().__init__()
+        self.lin1 = nn.Linear(input_dim, lin_dim)
+        self.lin2 = nn.Linear(lin_dim, lin_dim)
+        self.lin3 = nn.Linear(lin_dim, lin_dim)
+        self.lin4 = nn.Linear(lin_dim, lin_dim)
+        self.lin5 = nn.Linear(lin_dim, lin_dim)
+        self.out = nn.Linear(lin_dim, 2)
+
+    def forward(self, x):
+        y = F.relu(self.lin1(x))
+        y = F.relu(self.lin2(y))
+        y = F.relu(self.lin3(y))
+        y = F.relu(self.lin4(y))
+        y = F.relu(self.lin5(y))
+        y = F.log_softmax(self.out(y))
+        return y.squeeze()
+
+
+def train_batch(batch, s_encoder, classifier):
+    """Train the batch.
+    """
+    x, reorder = batch.packed_sentence_tensor()
+
+    # Encode sentences.
+    sents = s_encoder(x, reorder)
+
+    # Generate x / y pairs.
+    x, y = [], []
+    for ab in batch.unpack_sentences(sents):
+        for s1, s2 in pairwise(ab):
+
+            x.append(torch.cat([s1, s2]))
+            y.append(0)
+
+            x.append(torch.cat([s2, s1]))
+            y.append(1)
+
+    x = torch.stack(x)
+    y = Variable(torch.LongTensor(y)).type(itype)
+
+    return classifier(x), y
+
+
+def train(train_path, model_path, train_skim, lr, epochs, epoch_size,
+    batch_size, lstm_dim, lin_dim):
+    """Train model.
+    """
+    train = Corpus(train_path, train_skim)
+
+    s_encoder = Encoder(300, lstm_dim)
+    classifier = Classifier(2*lstm_dim, lin_dim)
+
+    params = (
+        list(s_encoder.parameters()) +
+        list(classifier.parameters())
+    )
+
+    optimizer = torch.optim.Adam(params, lr=lr)
+
+    loss_func = nn.NLLLoss()
+
+    if CUDA:
+        s_encoder = s_encoder.cuda()
+        classifier = classifier.cuda()
+
+    for epoch in range(epochs):
+
+        print(f'\nEpoch {epoch}')
+
+        epoch_loss, c, t = 0, 0, 0
+
+        for _ in tqdm(range(epoch_size)):
+
+            optimizer.zero_grad()
+
+            batch = train.random_batch(batch_size)
+
+            y_pred, y = train_batch(batch, s_encoder, classifier)
+
+            loss = loss_func(y_pred, y)
+            loss.backward()
+
+            optimizer.step()
+
+            epoch_loss += loss.data[0]
+
+            # EVAL
+
+            matches = (
+                np.argmax(y_pred.data.tolist(), 1) ==
+                np.array(y.data.tolist())
+            )
+
+            c += matches.sum()
+            t += len(matches)
+
+        print(epoch_loss / epoch_size)
+        print(c / t)
