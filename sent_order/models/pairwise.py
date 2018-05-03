9e045b33073f8a87b0590210c2c47b4de054b102
David McClure <davidwilliammcclure@gmail.com>
2018-05-03 07:51:37 -0400

    CONLL bindings for pairwise.

---
+++ sent_order/models/pairwise.py
@@ -0,0 +1,93 @@
+
+
+import attr
+import re
+
+from collections import defaultdict
+from cached_property import cached_property
+from boltons.iterutils import pairwise
+
+
+def parse_int(text):
+    """Parse an integer out of a string.
+    """
+    matches = re.findall('[0-9]+', text)
+    return int(matches[0]) if matches else None
+
+
+@attr.s
+class Token:
+
+    text = attr.ib()
+    document_id = attr.ib()
+    doc_index = attr.ib()
+    sent_index = attr.ib()
+    coref_id = attr.ib()
+
+
+class Document:
+
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+    def __repr__(self):
+        return 'Document<%d tokens>' % len(self.tokens)
+
+    def __len__(self):
+        return len(self.tokens)
+
+    @cached_property
+    def sent_start_indexes(self):
+        return [i for i, t in enumerate(self.tokens) if t.sent_index == 0]
+
+    def sents(self):
+        for i1, i2 in pairwise(self.sent_start_indexes + [len(self)]):
+            yield self.tokens[i1:i2]
+
+
+class GoldFile:
+
+    def __init__(self, path):
+        self.path = path
+
+    def lines(self):
+        """Split lines into cols. Skip comments / blank lines.
+        """
+        with open(self.path) as fh:
+            for line in fh:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    yield line.split()
+
+    def tokens(self):
+        """Generate tokens.
+        """
+        open_tag = None
+        for i, line in enumerate(self.lines()):
+
+            digit = parse_int(line[-1])
+
+            if digit is not None and line[-1].startswith('('):
+                open_tag = digit
+
+            yield Token(
+                text=line[3],
+                document_id=int(line[1]),
+                doc_index=i,
+                sent_index=int(line[2]),
+                coref_id=open_tag,
+            )
+
+            if line[-1].endswith(')'):
+                open_tag = None
+
+    def documents(self):
+        """Group tokens by document.
+        """
+        groups = defaultdict(list)
+
+        for token in self.tokens():
+            groups[token.document_id].append(token)
+
+        for tokens in groups.values():
+            yield Document(tokens)
